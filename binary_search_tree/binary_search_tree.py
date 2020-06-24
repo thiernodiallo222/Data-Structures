79f36881c41e53dcdd14bd3fc800dc930619672b
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-06-23 20:47:24 -0400

    almost done with first part of binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -20,7 +20,7 @@ class BSTNode:
         if self is None:
             self = BSTNode(value)
         else:
-            if value > self.value:
+            if value >= self.value:
                 if self.right is None:
                     self.right = BSTNode(value)
                 else:
@@ -36,49 +36,72 @@ class BSTNode:
     # False if it does not
     def contains(self, target):
         found = False
-        if self is None:
-            return found
-        if target == self.value:
-            found = True
-        elif target < self.value:
-            self.left.contains(target)
+        if self:
+            if target == self.value:
+                return True 
+            if target < self.value:
+                if self.left is None:
+                    return False
+                found = self.left.contains(target)
+            else:
+                if self.right is None:
+                    return False
+                found = self.right.contains(target)
         else:
-            self.right.contains(target) 
+             found = False
         return found
+        
         
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self:
+            if self.right:
+                return self.right.get_max()
+            return self.value
+            
+        else:
+            return None
+
 
     # Call the function `fn` on the value of each node
+    # call function on the current value fn(self.value)
+    # if you can go left, call for_each on the left tree
+    # if you can go right, call for_each on the right tree
     def for_each(self, fn):
-        pass
-
+        if self:
+            fn(self.value)
+            if self.left:
+                self.left.for_each()
+            elif self.right:
+                self.right.for_each()  
+        else:
+            return None 
+        
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    # def in_order_print(self, node):
+    #     pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
+    # # Print the value of every node, starting with the given node,
+    # # in an iterative breadth first traversal
+    # def bft_print(self, node):
+    #     pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
+    # # Print the value of every node, starting with the given node,
+    # # in an iterative depth first traversal
+    # def dft_print(self, node):
+    #     pass
 
-    # Stretch Goals -------------------------
-    # Note: Research may be required
+    # # Stretch Goals -------------------------
+    # # Note: Research may be required
 
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
+    # # Print Pre-order recursive DFT
+    # def pre_order_dft(self, node):
+    #     pass
 
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+    # # Print Post-order recursive DFT
+    # def post_order_dft(self, node):
+    #     pass
